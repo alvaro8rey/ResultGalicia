0574b5a8cedd0824c6bbee43dad9a48842cab550
Alvaro Rey <alvarorey7@gmail.com>
2026-04-30 22:33:55 +0200

    Initial Commit

---
+++ ResulGalicia/ResulGaliciaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResulGaliciaApp.swift
+//  ResulGalicia
+//
+//  Created by alvaro on 30/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ResulGaliciaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ResulGaliciaTests/ResulGaliciaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ResulGaliciaTests.swift
+//  ResulGaliciaTests
+//
+//  Created by alvaro on 30/04/2026.
+//
+
+import Testing
+@testable import ResulGalicia
+
+struct ResulGaliciaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
